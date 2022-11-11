16dcb76601631bf7a0ec50ffda50d6d3742f5f6f
unknown <inessalub@gmail.com>
2022-11-11 20:39:54 +0400

    Написать программу, которая обменивает элементы первой строки и последней строки

---
+++ example06/Program.cs
@@ -0,0 +1,49 @@
+﻿//Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Average(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine($"Среднее арифместическое  столбца {j + 1} равено:   {sum / (array.GetLength(0))}");
+    }
+}
+
+
+
+Console.WriteLine("Количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+Console.WriteLine("Массив: ");
+PrintArray(matrix);
+Average(matrix);
